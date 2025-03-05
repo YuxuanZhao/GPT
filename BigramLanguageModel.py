281da2bac2303d0209dbd0d963d923228bf618bc
YuxuanZhao <785315305@qq.com>
2025-03-04 23:49:52 -0500

    first stage

--- BigramLanguageModel.py
+++ BigramLanguageModel.py
@@ -1,6 +1,23 @@
-from torch.nn import Module
+from torch.nn import Module, Embedding, Linear
+from torch.nn.functional import cross_entropy
+from torch import arange
 
 class BigramLanguageModel(Module):
 
-    def __init__(self):
+    def __init__(self, vocab_size, embedding_size, window_size, batch_size, device):
         super().__init__()
+        self.vocab_size, self.embedding_size, self.window_size, self.batch_size, self.device = vocab_size, embedding_size, window_size, batch_size, device
+        self.token_embedding = Embedding(vocab_size, embedding_size)
+        self.position_embedding = Embedding(window_size, embedding_size)
+        self.head = Linear(embedding_size, vocab_size)
+
+    def forward(self, index, targets):
+        token_embedding = self.token_embedding(index) # (batch_size, window_size, embedding_size)
+        position_embedding = self.position_embedding(arange(self.window_size, device=self.device)) # (window_size, embedding_size)
+        representation = self.head(token_embedding + position_embedding) # (batch_size, window_size, vocab_size)
+
+        if not targets: return representation, None
+
+        representation.view(self.batch_size * self.window_size, self.vocab_size)
+        targets.view(self.batch_size * self.window_size)
+        return representation, cross_entropy(representation, targets)
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import Train
 import Test
 from torch.optim import AdamW
 
-model = BigramLanguageModel()
+model = BigramLanguageModel(vocab_size=100, embedding_size=384, window_size=256, batch_size=64, device='cuda')
 model.to('cuda')
 optimizer = AdamW(model.parameters(), lr=3e-4)
 

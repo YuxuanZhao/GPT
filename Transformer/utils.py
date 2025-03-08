5a3ea8fbd6b8be6f28f51c5b589b54e2cd3230c4
YuxuanZhao <785315305@qq.com>
2025-03-08 18:43:17 -0500

    explain

--- Transformer/utils.py
+++ Transformer/utils.py
@@ -2,7 +2,7 @@ from torch.nn import Module, Parameter, Linear, Dropout, ReLU, Sequential
 from torch import ones, zeros, sqrt
 
 # batchNorm 是对 batch 这个维度进行 normalize（不同的样本在同一个 channel 上分布应当均匀）
-# LayerNorm 是对 channel 这个维度进行 normalize（同一个样本在不同的 channel 上分布应当均匀）
+# LayerNorm 是对 channel 这个维度进行 normalize（同一个样本在不同的 channel 上分布应当均匀）对于语言输入的好处在于每个样本的长度会有差异，所以对不同样本取 variance 会有更大的波动
 class LayerNorm(Module):
     """Layer normalization over the channel dimension."""
     def __init__(self, embedding_size, epsilon=1e-10):
